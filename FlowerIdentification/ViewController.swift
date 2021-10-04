3dcc54a6a251a00fb5f8d508a97602ea291317c2
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-10-04 22:07:44 +0300

    add UIImage Picker function
    user can pick image from photo library

--- FlowerIdentification/ViewController.swift
+++ FlowerIdentification/ViewController.swift
@@ -12,12 +12,19 @@ import Vision
 
 class ViewController: UIViewController {
     
+    let pickerController = UIImagePickerController()
     
     
+    
+    @IBOutlet weak var flowerImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 
+        
+        pickerController.delegate = self // delegate UIImagePickerController
+        pickerController.allowsEditing = false // not allow edit image
+        pickerController.sourceType = .photoLibrary // only photo library use
+        
     }
     
     
@@ -28,12 +35,33 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func cameraBarButton(_ sender: UIBarButtonItem) {
+        
+        // activate imagePickerController
+        present(pickerController, animated: true, completion: nil)
+        
+    }
     
 }
 
 // MARK: -
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // get an image
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        // show chosen image on the screen
+        flowerImageView.image = image
+        
+        // dismiss picker
+        pickerController.dismiss(animated: true, completion: nil)
+        
+        
+        
+    }
     
 }
 
